963ccae7ad2212c9941a6b586094a6d9bb6431a1
cludley <cludley@anamosa.k12.ia.us>
2025-11-18 19:12:29 +0000

    h

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function calculateAward(points) {
+    if (points > 99) {
+        return "First Place";
+    } else if (points > 49) {
+        return "Second Place";
+    } else {
+        return "Participation Award";
+    }
+}
